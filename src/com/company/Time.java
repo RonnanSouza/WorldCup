d722440f5490e470e61553e0816797f209f0bef0
Ronan Souza <ronan.souza@ccc.ufcg.edu.br>
2019-11-17 11:06:17 -0300

    adding some docs to time entity

--- src/com/company/Time.java
+++ src/com/company/Time.java
@@ -1,10 +1,25 @@
 package com.company;
 
+/**
+ * Time é uma entidade que representa cada entrada do CSV
+ */
 public class Time {
 
     private int jogos, vitorias, empates, derrotas, pontos, golsMarcados, golsSofridos;
     private String grupo, nome;
 
+
+    /**
+     *
+     * @param grupo grupo que esta equipe faz parte no campeonato, é representada por uma letra no arquivo CSV
+     * @param nome nome da equipe
+     * @param jogos número de jogos
+     * @param vitorias número de vitórias da equipe na competição
+     * @param empates número de empates da equipe na competição
+     * @param derrotas número de derrotas da equipe na competição
+     * @param golsMarcados número de gols marcados pela equipe na competição
+     * @param golsSofridos número de gols sofridos pela equipe na competição
+     */
 
     public Time(String grupo, String nome, int jogos, int vitorias, int empates, int derrotas, int golsMarcados,
                 int golsSofridos) {
@@ -19,12 +34,12 @@ public class Time {
         this.pontos = this.vitorias * 3 + this.empates;
     }
 
-    public String getGrupo() {
-        return grupo;
+    public int getJogos() {
+        return jogos;
     }
 
-    public void setGrupo(String grupo) {
-        this.grupo = grupo;
+    public void setJogos(int jogos) {
+        this.jogos = jogos;
     }
 
     public int getVitorias() {
@@ -75,6 +90,14 @@ public class Time {
         this.golsSofridos = golsSofridos;
     }
 
+    public String getGrupo() {
+        return grupo;
+    }
+
+    public void setGrupo(String grupo) {
+        this.grupo = grupo;
+    }
+
     public String getNome() {
         return nome;
     }
@@ -83,11 +106,13 @@ public class Time {
         this.nome = nome;
     }
 
-    public int getJogos() {
-        return jogos;
+    /**
+     *
+     * @return Nome da equipe seguido pelo número de pontos da equipe na competição
+     */
+    @Override
+    public String toString() {
+        return String.format("%s, pts: %d", this.getNome(), this.getPontos());
     }
 
-    public void setJogos(int jogos) {
-        this.jogos = jogos;
-    }
 }
